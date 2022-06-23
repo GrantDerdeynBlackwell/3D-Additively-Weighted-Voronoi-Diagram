84f808e85c518bbc72fc3e6f3d6f9f3158caaedb
Grant Derdeyn-Blackwell <grantderdeynblackwell0@protonmail.com>
2022-06-23 12:59:50 -0400

    changed cleanup_meshes.sh to accept a directory as an argument

--- cleanup_meshes.sh
+++ cleanup_meshes.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
-sed -i 's/COFF/OFF/' $1/*.off
-awk -i inplace '{printf $1=="" ? "\n" : "%s %s %s %s", $1, $2, $3, $4 ;  printf $5=="" ? "\n" : " %.3g %.3g %.3g\n", $5 / 255, $6 / 255, $7 / 255 }' outputs/*.off
+dir=${1:-.}
+
+sed -i 's/COFF/OFF/' $dir/*.off
+awk -i inplace '{printf $1=="" ? "\n" : "%s %s %s %s", $1, $2, $3, $4 ;  printf $5=="" ? "\n" : " %.3g %.3g %.3g\n", $5 / 255, $6 / 255, $7 / 255 }' $dir/*.off
